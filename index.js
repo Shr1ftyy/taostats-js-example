5a7ecdb1b1a1e9a7b9cd081d6ae05a1b2dbefeaa
shr1ftyy <syeamtechdemon@gmail.com>
2025-03-25 13:32:37 +1030

    feat: taostats js example getting apy

---
+++ index.js
@@ -0,0 +1,37 @@
+import taostats from '@api/taostats';
+import 'dotenv/config';
+
+async function main() {
+    taostats.auth(process.env.TAOSTATS_API_KEY);
+    const validatorData = await taostats.getValidatorDtao({
+        hotkey: '5GKH9FPPnWSUoeeTJp19wVtd84XqFW4pyK2ijV2GsFbhTrP1'
+    });
+
+    const validatorDataWithLimit = await taostats.getValidatorHistoryDtao({
+        hotkey: '5GKH9FPPnWSUoeeTJp19wVtd84XqFW4pyK2ijV2GsFbhTrP1',
+        limit: 7
+    });
+
+    // get validator's current apy
+    const currentData = validatorData.data.data[0];
+
+    // get validator's apy for the last 7 days
+    // console.log("Validator Data from the last 7 days");
+    const sevenDaysData = validatorDataWithLimit.data.data.slice(0, 7);
+    // console.log(sevenDaysData);
+
+    // iterate through the data and calculate the average apy also display each apy for the last 7 days
+    let totalApy = 0;
+    sevenDaysData.forEach((data, index) => {
+        const apy = (data.nominator_return_per_day / data.root_stake) * 365 * 100;
+        totalApy += apy;
+        console.log(`Day ${index + 1} APY: ${apy}%`);
+    });
+    const averageApy = totalApy / sevenDaysData.length;
+    console.log(`Average APY for the last 7 days: ${averageApy}%`);
+
+    const apy = (currentData.nominator_return_per_day / currentData.root_stake) * 365 * 100;
+    console.log(`Validator's current APY: ${apy}%`);
+}
+
+await main()
